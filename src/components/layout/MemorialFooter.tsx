2ab851e3e90c85a4a5944762398f3b7d78003357
Brett Gadd <brett.gadd@odb.org>
2025-09-04 21:40:02 -0700

    updates

--- src/components/layout/MemorialFooter.tsx
+++ src/components/layout/MemorialFooter.tsx
@@ -14,7 +14,7 @@ export function MemorialFooter() {
         ext: ".svg",
       });
     }
-    
+
     // Trigger animation after a short delay
     const timer = setTimeout(() => {
       setIsVisible(true);
@@ -28,13 +28,15 @@ export function MemorialFooter() {
       <div className="flex justify-center pt-28">
         <p className="text-base text-white/50">
           Built with{" "}
-          <span 
-            ref={heartRef} 
+          <span
+            ref={heartRef}
             className={`inline-block h-4 w-4 transition-all duration-1000 ease-out ${
-              isVisible ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0'
+              isVisible
+                ? "translate-y-0 opacity-100"
+                : "translate-y-8 opacity-0"
             }`}
-          ></span> by his
-          family in {new Date().getFullYear()}
+          ></span>{" "}
+          by Gene&apos;s family in {new Date().getFullYear()}
         </p>
       </div>
     </footer>
